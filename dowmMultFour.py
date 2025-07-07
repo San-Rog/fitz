d90a3e205db81a859748b8db8c8b2b1e5662e9e4
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-07-07 20:23:52 -0300

    Add files via upload

---
+++ dowmMultFour.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import zipfile
+import io
+
+def compress_files(uploaded_files):
+    zip_buffer = io.BytesIO()
+    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
+        for uploaded_file in uploaded_files:
+            zip_file.writestr(uploaded_file.name, uploaded_file.read())
+    zip_buffer.seek(0)
+    return zip_buffer
+
+st.title("Compactador de Arquivos Streamlit")
+uploaded_files = st.file_uploader('Selecionar arquivos PDF', type=['pdf'], accept_multiple_files=True)
+if uploaded_files:
+    if st.button("Compactar"):
+        try:
+            zip_file = compress_files(uploaded_files)
+            st.download_button(
+                label="Baixar arquivo ZIP",
+                data=zip_file,
+                file_name="compactado.zip",
+                mime="application/zip"
+            )
+        except Exception as e:
+            st.error(f"Erro ao compactar os arquivos: {e}")
