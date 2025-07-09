55d1797ca6f817f8952ac48a25d2fb14fb65818a
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-07-09 08:40:50 -0300

    Update dowmMultFour.py

--- dowmMultFour.py
+++ dowmMultFour.py
@@ -26,4 +26,4 @@ if uploaded_files:
         except Exception as e:
             st.error(f"Erro ao compactar os arquivos: {e}")
 text_calendar = calendar.TextCalendar()
-st.write(text_calendar.pryear(2024))
+st.text(text_calendar.pryear(2024))
